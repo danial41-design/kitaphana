a1fe15f978a6a2e95948cd89eeb03f61f7018806
kanaryth <rashirbek123.kz@gmail.com>
2025-01-21 23:14:00 +0500

    Update IDB.java

--- src/data/interfaces/IDB.java
+++ src/data/interfaces/IDB.java
@@ -1,4 +1,11 @@
 package data.interfaces;
 
+public interface IDB {package data.interfaces;
+
+import java.sql.Connection;
+
 public interface IDB {
+    Connection getConnection();
+    void close();
+}
 }
