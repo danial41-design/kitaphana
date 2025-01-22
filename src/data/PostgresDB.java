94cdddc3cc57c86dabb58de5869536d18e739471
yernar0 <ernarorleu9988@gmail.com>
2025-01-22 12:15:01 +0500

    PostgresDB.java

--- src/data/PostgresDB.java
+++ src/data/PostgresDB.java
@@ -1,63 +1,80 @@
-package data;
+package database;
 
-public class PostgresDB {import data.PostgresDB;
+import database.interfaces.IDB;
 import java.sql.Connection;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        PostgresDB postgresDB = new PostgresDB(
-                "jdbc:postgresql://localhost:5432",
-                "postgres",
-                "4584",
-                "library"
-        );
-        Connection connection = postgresDB.getConnection();
-        DatabaseInitializer.initializeDatabase(connection);
-        LibraryDB libraryDB = new LibraryDB(postgresDB.getConnection());
-        LibraryService libraryService = new LibraryService(libraryDB);
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("\n1. show all books");
-            System.out.println("2. get books");
-            System.out.println("3. give a book");
-            System.out.println("4. Add a new book");
-            System.out.println("5. close");
-            System.out.print("select action: ");
-            int option = scanner.nextInt();
-            switch (option) {
-                case 1:
-                    libraryService.showAllBooks();
-                    break;
-                case 2:
-                    System.out.print("input ID: ");
-                    int takeId = scanner.nextInt();
-                    libraryService.takeBook(takeId);
-                    break;
-                case 3:
-                    System.out.print("input ID: ");
-                    int returnId = scanner.nextInt();
-                    libraryService.returnBook(returnId);
-                    break;
-                case 4:
-                    System.out.print("name of book: ");
-                    scanner.nextLine();
-                    String name = scanner.nextLine();
-                    System.out.print("autor of book: ");
-                    String author = scanner.nextLine();
-                    System.out.print("year of book: ");
-                    int year = scanner.nextInt();
-                    System.out.print("number of books: ");
-                    int quantity = scanner.nextInt();
-                    libraryService.addNewBook(name, author, year, quantity);
-                    break;
-                case 5:
-                    postgresDB.close();
-                    System.out.println("code is closed.");
-                    return;
-                default:
-                    System.out.println("wrong option, please try again.");
-            }
-        }
-    }
-}
+import java.sql.DriverManager;
+
+public class PostgreDB implements IDB {
+	private String host;
+	private String username;
+	private String password;
+	private String dbname;
+
+	private Connection connection;
+
+	public PostgreDB(String host, String username, String password, String dbname) {
+		setHost(host);
+		setUsername(username);
+		setPassword(password);
+		setDbname(dbname);
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getDbname() {
+		return dbname;
+	}
+	public void setDbname(String dbname) {
+		this.dbname = dbname;
+	}
+
+	@Override
+	public Connection getConnection() {
+		String connectionUrl = host + "/" + dbname;
+		try {
+			if (connection != null && !connection.isClosed()) {
+				return connection;
+			}
+
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection(connectionUrl, username, password);
+			System.out.println("Connection successful.");
+			return connection;
+		} catch (Exception e) {
+			System.out.println("Connection is failed: " + e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+
+	@Override
+	public void close() {
+		if (connection != null){
+			try {
+				connection.close();
+			}catch (Exception e){
+				System.out.println("Failed to close database connection: " + e.getMessage());
+			}
+		}
+	}
+
 }
