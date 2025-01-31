2ab4b67e8b9215d376b5973f47a729d4eb8a78fa
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-01-31 13:29:35 +0500

    ReturnBookMenuOption.java

---
+++ src/menu/options/ReturnBookMenuOption.java
@@ -0,0 +1,26 @@
+package menu.options;
+
+import menu.MenuOption;
+import controllers.BookController;
+import java.util.Scanner;
+
+public class ReturnBookMenuOption implements MenuOption {
+    private BookController bookController;
+
+    public ReturnBookMenuOption(BookController bookController) {
+        this.bookController = bookController;
+    }
+
+    @Override
+    public void execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the book title to return: ");
+        String title = scanner.nextLine();
+        bookController.returnBook(title); 
+    }
+
+    @Override
+    public String getDescription() {
+        return "Return a book";
+    }
+}
