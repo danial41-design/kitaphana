604621499e78776dd1264f9be471d2e7259ebc39
danial41-design <121389931+danial41-design@users.noreply.github.com>
2025-01-31 08:20:05 +0500

    Update LibraryService.java

--- src/LibraryService.java
+++ src/LibraryService.java
@@ -1,56 +1,14 @@
+package wrongcode2;
+
+import java.sql.SQLException;
 import java.util.List;
-import java.util.Optional;
 
-public class LibraryService {
-    private LibraryDB libraryDB;
-
-    public LibraryService(LibraryDB libraryDB) {
-        this.libraryDB = libraryDB;
-    }
-
-    public void showAllBooks() {
-        List<Book> books = libraryDB.getAllBooks();
-        for (Book book : books) {
-            System.out.println(book);
-        }
-    }
-
-    public void takeBook(int id) {
-        List<Book> books = libraryDB.getAllBooks();
-        Optional<Book> bookOpt = books.stream().filter(book -> book.getId() == id).findFirst();
-
-        if (bookOpt.isPresent()) {
-            Book book = bookOpt.get();
-            if (book.getQuantity() > 0) {
-                libraryDB.updateBookQuantity(id, book.getQuantity() - 1);
-                System.out.println("You got: " + book.getName());
-            } else {
-                System.out.println("This book isn't available.");
-            }
-        } else {
-            System.out.println("Book not found.");
-        }
-    }
-
-    public void returnBook(int id) {
-        List<Book> books = libraryDB.getAllBooks();
-        Optional<Book> bookOpt = books.stream().filter(book -> book.getId() == id).findFirst();
-
-        if (bookOpt.isPresent()) {
-            Book book = bookOpt.get();
-            libraryDB.updateBookQuantity(id, book.getQuantity() + 1);
-            System.out.println("You gave book: " + book.getName());
-        } else {
-            System.out.println("Book not found.");
-        }
-    }
-    public void addNewBook(String name, String author, int year, int quantity) {
-        boolean success = libraryDB.addBook(name, author, year, quantity);
-        if (success) {
-            System.out.println("Book added successfully: " + name);
-        } else {
-            System.out.println("Book doesn't added.");
-        }
-    }
-
+public interface LibraryService {
+	List<Book> getAllBooks() throws SQLException, SQLException;
+	List<User> getAllUsers() throws SQLException, SQLException;
+	void takeBook(String name, String surname, String bookTitle) throws SQLException;
+	void returnBook(String name, String surname, String bookTitle) throws SQLException;
+	void addNewBook(String title, String author, int year, int quantity) throws SQLException;
 }
+
+
