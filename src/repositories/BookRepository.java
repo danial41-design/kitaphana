bba8cc7b8e061b64fb03b695006fa84368f93c8c
danial41-design <121389931+danial41-design@users.noreply.github.com>
2025-01-31 13:25:14 +0500

    Update BookRepository.java

--- src/repositories/BookRepository.java
+++ src/repositories/BookRepository.java
@@ -12,11 +12,9 @@ public class BookRepository {
     public List<Book> getAllBooks() throws SQLException {
         List<Book> books = new ArrayList<>();
         String query = "SELECT * FROM librarydb ORDER BY id ASC";
-
         try (Connection conn = db.getConnection();
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(query)) {
-
             while (rs.next()) {
                 books.add(new Book(
                         rs.getInt("id"),
@@ -30,9 +28,9 @@ public class BookRepository {
         return books;
     }
 
+    // Добавление новой книги
     public void addNewBook(String title, String author, int year, int quantity) throws SQLException {
         String query = "INSERT INTO librarydb (title, author, year, quantity) VALUES (?, ?, ?, ?)";
-
         try (Connection conn = db.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(query)) {
             pstmt.setString(1, title);
@@ -45,7 +43,24 @@ public class BookRepository {
 
     public void deleteBook(String bookTitle) throws SQLException {
         String query = "DELETE FROM librarydb WHERE title = ?";
+        try (Connection conn = db.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, bookTitle);
+            pstmt.executeUpdate();
+        }
+    }
 
+    public void takeBook(String bookTitle) throws SQLException {
+        String query = "UPDATE librarydb SET quantity = quantity - 1 WHERE title = ? AND quantity > 0";
+        try (Connection conn = db.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, bookTitle);
+            pstmt.executeUpdate();
+        }
+    }
+
+    public void returnBook(String bookTitle) throws SQLException {
+        String query = "UPDATE librarydb SET quantity = quantity + 1 WHERE title = ?";
         try (Connection conn = db.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(query)) {
             pstmt.setString(1, bookTitle);
@@ -53,3 +68,4 @@ public class BookRepository {
         }
     }
 }
+
